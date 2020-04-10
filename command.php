7fff500fd07a86a3d2a04b82f2d30811113fd7c1
Sandeep Kumar <sandeepkumar.skv3@gmail.com>
2020-04-10 18:09:33 +0530

    index.html and style.css added

---
+++ command.php
@@ -0,0 +1,23 @@
+echo "# GithubLearning" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/sandeep-skv/GithubLearning.git
+git push -u origin master
+                
+git remote add origin https://github.com/sandeep-skv/GithubLearning.git
+git push -u origin master
+
+
+git config --global user.email "sandeepkumar.skv3@gmail.com"
+git config --global user.name "sandeep kumar"
+
+Learn git Part -1 :
+1. git init
+2. git add [filename] || -A
+3. git status
+4. git commit -m "msg"
+5. git remote add origin repoName
+6. git push origin master
+7. git clone repoName
+8. git pull origin master
